556fe3ecfc769d90fe7631b144bf051afb7b871c
Raymo0n <ramon.gomm8@gmail.com>
2024-12-07 18:50:55 +0100

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,52 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        input.forEach {
+            val split = it.trim().split(" ", limit = 2);
+            list1.add(split[0].toInt());
+            list2.add(split[1].trim().toInt());
+        }
+        list1.sort()
+        list2.sort()
+        var diff = 0;
+        for (i in list1.indices) {
+            diff += abs(list1[i] - list2[i])
+        }
+
+        return diff
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        input.forEach {
+            val split = it.trim().split(" ", limit = 2);
+            list1.add(split[0].toInt());
+            list2.add(split[1].trim().toInt());
+        }
+        val summary1 = list1.groupBy { it }.mapValues { it.value.size }
+        val summary2 = list2.groupBy { it }.mapValues { it.value.size }
+
+        var similarity = 0
+        for (i in summary1) {
+            similarity += summary2[i.key]?.let { i.key * it * i.value } ?: 0
+        }
+        return similarity
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    val input1 = """3   4
+                4   3
+                2   5
+                1   3
+                3   9
+                3   3""".split("\n")
+    check(part1(input1) == 11)
+    check(part2(input1) == 31)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Day1")
     part1(input).println()
     part2(input).println()
 }
