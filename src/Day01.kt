bae474db2ebd089c16dedb67831bd329406a8550
Raymo0n <ramon.gomm8@gmail.com>
2024-12-07 19:32:22 +0100

    improve day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,40 +1,30 @@
 import kotlin.math.abs
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        val list1 = mutableListOf<Int>()
-        val list2 = mutableListOf<Int>()
-        input.forEach {
-            val split = it.trim().split(" ", limit = 2);
-            list1.add(split[0].toInt());
-            list2.add(split[1].trim().toInt());
-        }
-        list1.sort()
-        list2.sort()
-        var diff = 0;
-        for (i in list1.indices) {
-            diff += abs(list1[i] - list2[i])
-        }
 
-        return diff
+    fun getTwoList(input: List<String>) = input
+        .map { it.trim() }
+        .map {
+            val left = it.substringBefore(" ").toLong()
+            val right = it.substringAfterLast(" ").toLong()
+            left to right
+        }.unzip()
+
+    fun part1(input: List<String>): Long {
+        val (left, right) = getTwoList(input)
+        return left.sorted()
+            .zip(right.sorted())
+            .sumOf { (a, b) -> abs(a - b) }
     }
 
-    fun part2(input: List<String>): Int {
-        val list1 = mutableListOf<Int>()
-        val list2 = mutableListOf<Int>()
-        input.forEach {
-            val split = it.trim().split(" ", limit = 2);
-            list1.add(split[0].toInt());
-            list2.add(split[1].trim().toInt());
-        }
-        val summary1 = list1.groupBy { it }.mapValues { it.value.size }
-        val summary2 = list2.groupBy { it }.mapValues { it.value.size }
+    fun part2(input: List<String>): Long {
+        val (left, right) = getTwoList(input)
 
-        var similarity = 0
-        for (i in summary1) {
-            similarity += summary2[i.key]?.let { i.key * it * i.value } ?: 0
-        }
-        return similarity
+        val summary1 = left.groupingBy { it }.eachCount()
+        val summary2 = right.groupingBy { it }.eachCount()
+
+        return summary1.map { (num, amount) -> summary2[num]?.let { num * amount * it } ?: 0 }
+            .sum()
     }
 
     val input1 = """3   4
@@ -43,8 +33,8 @@ fun main() {
                 1   3
                 3   9
                 3   3""".split("\n")
-    check(part1(input1) == 11)
-    check(part2(input1) == 31)
+    check(part1(input1) == 11L)
+    check(part2(input1) == 31L)
 
     val input = readInput("Day1")
     part1(input).println()
